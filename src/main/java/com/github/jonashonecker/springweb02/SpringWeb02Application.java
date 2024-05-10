d4a6997484af01dd9c9425ca6c96b48db48c5add
Jonas Honecker <honeckerjonas@gmail.com>
2024-05-10 11:29:38 +0200

    Initial commit

---
+++ src/main/java/com/github/jonashonecker/springweb02/SpringWeb02Application.java
@@ -0,0 +1,13 @@
+package com.github.jonashonecker.springweb02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeb02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWeb02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jonashonecker/springweb02/SpringWeb02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jonashonecker.springweb02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeb02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
